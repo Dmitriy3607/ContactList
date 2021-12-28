9870a526eb41da6607a985c90368d3a92cbcc5e1
Dmitriy Shulgin <dm.android@gmail.com>
2021-12-28 23:38:09 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Дмитрий Шульгин on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
